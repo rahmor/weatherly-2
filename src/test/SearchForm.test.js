f98ed58940d52efd527551a2e7bc0feb1bbcc082
rahmor <rahmor@yahoo.com>
2020-12-02 13:44:24 -0500

    update SearchForm tests

--- src/test/SearchForm.test.js
+++ src/test/SearchForm.test.js
@@ -3,26 +3,36 @@ import React from 'react';
 import moment from 'moment';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import { after } from 'lodash';
 jest.mock('moment', () => {
   return () => jest.requireActual('moment')('2020-01-01T00:00:00.000Z');
 });
 
 describe('SearchForm component', () => {
   const props = { fetchCoordinates: jest.fn(), city: 'New York' };
+  const wrapper = shallow(<SearchForm {...props} />);
+
+  afterAll(() => {
+    wrapper.unmount();
+    wrapper.debug();
+  });
 
   it('should match snapshot', () => {
-    const wrapper = shallow(<SearchForm {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
-    wrapper.unmount();
+  });
+
+  describe('when span clicked', () => {
+    it('should match snapshot and show input element', () => {
+      wrapper.find('span').simulate('click');
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
   });
 
   it('should call renderProps', () => {
-    const wrapper = shallow(<SearchForm {...props} />);
     wrapper
       .find('form')
       .at(0)
       .simulate('submit', { preventDefault: jest.fn() });
     expect(props.fetchCoordinates).toHaveBeenCalled();
-    wrapper.unmount();
   });
 });
